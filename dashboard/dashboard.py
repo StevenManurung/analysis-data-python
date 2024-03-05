fd0bf9dea3765d92ae94a5d17e2b6ea183fb762f
Steven Manurung <94947108+StevenManurung@users.noreply.github.com>
2024-03-06 00:47:26 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,11 +10,11 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Databases
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-df_combine = pd.read_csv("combine_data.csv")
+df_combine = pd.read_csv("dashboard/combine_data.csv")
 df_combine.sort_values(by="order_approved_at", inplace=True)
 df_combine.reset_index(inplace=True)
 
-geolocation = pd.read_csv('geolocation_dataset.csv')
+geolocation = pd.read_csv('dashboard/geolocation_dataset.csv')
 
 for col in datetime_cols:
     df_combine[col] = pd.to_datetime(df_combine[col])
