282625931619c9c749eda158b7a8630fb67f7f91
StevenManurung <Steven_mrng43@gmail.com>
2024-03-05 08:54:30 +0700

    first-commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,23 +1,20 @@
+# Steven Manurung
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import seaborn as sns
-import streamlit as st
-import urllib
-from func import DataAnalyzer, BrazilMapPlotter
-from babel.numbers import format_currency
-sns.set(style='dark')
+
+from function_all import DataAnalyzer
+sns.set(style='white')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Dataset
+# Databases
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-df_combine = pd.read_csv("./databases/combine_data.csv")
+df_combine = pd.read_csv("../databases/combine_data.csv")
 df_combine.sort_values(by="order_approved_at", inplace=True)
 df_combine.reset_index(inplace=True)
 
-# Geolocation Dataset
-geolocation = pd.read_csv('./databases/geolocation.csv')
-data = geolocation.drop_duplicates(subset='customer_unique_id')
+geolocation = pd.read_csv('../databases/geolocation_dataset.csv')
 
 for col in datetime_cols:
     df_combine[col] = pd.to_datetime(df_combine[col])
@@ -30,12 +27,9 @@ with st.sidebar:
     # Title
     st.title("Steven Manurung")
 
-    # Logo Image
-    st.image("./dashboard/gcl.png")
-
     # Date Range
     start_date, end_date = st.date_input(
-        label="Select Date Range",
+        label="Pilih jarak tanggal",
         value=[min_date, max_date],
         min_value=min_date,
         max_value=max_date
@@ -46,7 +40,6 @@ main_df = df_combine[(df_combine["order_approved_at"] >= str(start_date)) &
                  (df_combine["order_approved_at"] <= str(end_date))]
 
 function = DataAnalyzer(main_df)
-map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
 
 daily_orders_df = function.create_daily_orders_df()
 sum_spend_df = function.create_sum_spend_df()
@@ -56,103 +49,45 @@ state, most_common_state = function.create_bystate_df()
 order_status, common_status = function.create_order_status()
 
 # Title
-st.header("E-Commerce Dashboard :convenience_store:")
-
-# Daily Orders
-st.subheader("Daily Orders")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    total_order = daily_orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
-
-with col2:
-    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Revenue: **{total_revenue}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(
-    daily_orders_df["order_approved_at"],
-    daily_orders_df["order_count"],
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
-
-# Customer Spend Money
-st.subheader("Customer Spend Money")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Spend: **{total_spend}**")
-
-with col2:
-    avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
-    st.markdown(f"Average Spend: **{avg_spend}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(
-    sum_spend_df["order_approved_at"],
-    sum_spend_df["total_spend"],
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
+st.header("E-Commerce Dashboard")
 
 # Order Items
-st.subheader("Order Items")
+st.subheader("Item Paling banyak diminati")
 col1, col2 = st.columns(2)
 
 with col1:
     total_items = sum_order_items_df["product_count"].sum()
     st.markdown(f"Total Items: **{total_items}**")
 
-with col2:
-    avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(45, 25))  # Create a single subplot
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+colors = ["#068DA9"]
 
-colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=30)
-ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
-ax[0].tick_params(axis ='y', labelsize=35)
-ax[0].tick_params(axis ='x', labelsize=30)
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=30)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=50)
-ax[1].tick_params(axis='y', labelsize=35)
-ax[1].tick_params(axis='x', labelsize=30)
+sns.barplot(
+    x="product_count",
+    y="product_category_name_english",
+    data=sum_order_items_df.head(3),
+    palette=colors,
+    ax=ax,
+)
+ax.set_ylabel(None)
+ax.set_xlabel("Jumlah penjual", fontsize=30)
+ax.set_title("Produk paling banyak terjual", loc="center", fontsize=50)
+ax.tick_params(axis='y', labelsize=35)
+ax.tick_params(axis='x', labelsize=30)
 
 st.pyplot(fig)
 
 # Review Score
-st.subheader("Review Score")
+st.subheader("Penilian pelanggan kepada pelayanan")
 col1,col2 = st.columns(2)
 
 with col1:
     avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score}**")
+    st.markdown(f"Rata-rata skor penialian: **{avg_review_score}**")
 
 with col2:
     most_common_review_score = review_score.value_counts().index[0]
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
 
 fig, ax = plt.subplots(figsize=(12, 6))
 sns.barplot(x=review_score.index, 
@@ -161,19 +96,18 @@ sns.barplot(x=review_score.index,
             palette=["#068DA9" if score == common_score else "#D3D3D3" for score in review_score.index]
             )
 
-plt.title("Rating by customers for service", fontsize=15)
-plt.xlabel("Rating")
-plt.ylabel("Count")
+plt.title("Penilaian customer tentang pelayanan", fontsize=15)
+plt.xlabel("Penilaian")
+plt.ylabel("Jumlah")
 plt.xticks(fontsize=12)
 st.pyplot(fig)
 
 # Customer Demographic
-st.subheader("Customer Demographic")
-tab1, tab2, tab3 = st.tabs(["State", "Order Status", "Geolocation"])
+st.subheader("Pelanggan dalam negara")
+tab1, tab2 = st.tabs(["State", "Order Status"])
 
 with tab1:
     most_common_state = state.customer_state.value_counts().index[0]
-    st.markdown(f"Most Common State: **{most_common_state}**")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=state.customer_state.value_counts().index,
@@ -182,15 +116,14 @@ with tab1:
                 palette=["#068DA9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
                     )
 
-    plt.title("Number customers from State", fontsize=15)
-    plt.xlabel("State")
-    plt.ylabel("Number of Customers")
+    plt.title("Jumlah customer dalam negara", fontsize=15)
+    plt.xlabel("Negara")
+    plt.ylabel("Jumlah customer")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
 
 with tab2:
     common_status_ = order_status.value_counts().index[0]
-    st.markdown(f"Most Common Order Status: **{common_status_}**")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=order_status.index,
@@ -199,16 +132,8 @@ with tab2:
                 palette=["#068DA9" if score == common_status else "#D3D3D3" for score in order_status.index]
                 )
     
-    plt.title("Order Status", fontsize=15)
+    plt.title("Status Order", fontsize=15)
     plt.xlabel("Status")
-    plt.ylabel("Count")
+    plt.ylabel("Jumlah")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
-
-with tab3:
-    map_plot.plot()
-
-    with st.expander("See Explanation"):
-        st.write('Sesuai dengan grafik yang sudah dibuat, ada lebih banyak pelanggan di bagian tenggara dan selatan. Informasi lainnya, ada lebih banyak pelanggan di kota-kota yang merupakan ibu kota (São Paulo, Rio de Janeiro, Porto Alegre, dan lainnya).')
-
-st.caption('Copyright (C) Sulistiawan A. P. 2023')
